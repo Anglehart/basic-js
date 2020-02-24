af28228f6885c897dd7d04f10fe418b917f6c561
Boris Kobzarev <planez@bk.ru>
2020-02-25 01:14:53 +0300

    refactor: change structure of dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,16 +1,17 @@
 module.exports = function createDreamTeam(members) {
   let dreamTeam = [];
-  if (!Array.isArray(members)){
-    return false;
-  }
-  for (let i = 0; i < members.length; i++){
-    if (typeof members[i] === "string"){
-      dreamTeam.push(members[i].trim().toUpperCase()[0]);
+  if (Array.isArray(members)){
+    for (let i = 0; i < members.length; i++){
+      if (typeof members[i] === "string"){
+        dreamTeam.push(members[i].trim().toUpperCase()[0]);
+      }
     }
-  }
-  if (dreamTeam != []){
-    return dreamTeam.sort().join("");
+    if (dreamTeam != []){
+      return dreamTeam.sort().join("");
+    } else {
+      return false
+    }
   } else {
-    return false
+    return false;
   }
 };
