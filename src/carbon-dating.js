8acce5449a8768f2351ff8074f0a6fba22aafa41
Boris Kobzarev <planez@bk.ru>
2020-02-24 22:57:09 +0300

    feat: carbon-dating done

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,12 @@
-const MODERN_ACTIVITY= 15; 
+const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(samAc) {
+  if (typeof samAc === "string" && /^[0-9.]{1,}$/.test(samAc) && parseFloat(samAc) > 0 && parseFloat(samAc) < 15) {
+    let k = 0.693/HALF_LIFE_PERIOD
+    let years = Math.ceil(Math.log(MODERN_ACTIVITY/parseFloat(samAc))/k)
+    return years
+  } else {
+    return false
+  }
 };
