780a706b5e90db91cffecf9c07222950b2fb731d
Anglehart <planez@bk.ru>
2020-02-25 11:27:39 +0300

    feat: what-season done

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,15 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (typeof date == "undefined") {
+    return 'Unable to determine the time of year!'
+  } if (isNaN(date)) {
+    throw new Error()
+  } else if (date.getMonth() < 2 || date.getMonth() == 11) {
+    return "winter"
+  } else if (date.getMonth() >=2 && date.getMonth() < 5) {
+    return "spring"
+  } else if (date.getMonth() >=5 && date.getMonth() < 8) {
+    return "summer"
+  } else if (date.getMonth() >=8 && date.getMonth() < 11) {
+    return "fall"
+  }
 };
